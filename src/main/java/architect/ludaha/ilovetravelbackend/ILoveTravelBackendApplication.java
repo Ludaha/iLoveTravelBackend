9762b33775a517d29600cd1dfeda45fd270ebe20
LuisRz1 <langulor1@upao.edu.pe>
2025-01-02 18:15:48 -0500

    first commit

---
+++ src/main/java/architect/ludaha/ilovetravelbackend/ILoveTravelBackendApplication.java
@@ -0,0 +1,13 @@
+package architect.ludaha.ilovetravelbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ILoveTravelBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ILoveTravelBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/architect/ludaha/ilovetravelbackend/ILoveTravelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package architect.ludaha.ilovetravelbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ILoveTravelBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

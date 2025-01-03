b299431d5061f96a296d642f71664269f3435194
LuisRz1 <langulor1@upao.edu.pe>
2025-01-02 19:03:43 -0500

    Hexagonal Architecture: Package Structure

--- src/test/java/architect/ludaha/ilovetravelbackend/ILoveTravelBackendApplicationTests.java
+++ src/test/java/architect/ludaha/ilovetravelbackend/ILoveTravelBackendApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ILoveTravelBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
